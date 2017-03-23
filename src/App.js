6ab9ad61f64718c683f8e223ffaef1ec3d0a278b
Vladimir Tyalo <bobtial@inbox.ru>
2017-03-23 14:41:56 +0400

    feat(buttons) add button styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Display from './Display';
+import InputArea from './InputArea';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1 className="title">Calculator</h1>
+        <div className="calculator">
+          <Display />
+          <InputArea />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
